5e29e42bde0e1e766fbfcefeb484e4adaf1c035b
VilleLipsanen <t7livi00@students.oamk.fi>
2019-05-13 10:08:32 +0300

    Updated CharController and FixedCamera

--- HackNSlash/Assets/Scripts/Player Controller/CharController.cs
+++ HackNSlash/Assets/Scripts/Player Controller/CharController.cs
@@ -1,48 +1,87 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CharController : MonoBehaviour
 {
+    public float speed = 6f;            // The speed that the player will move at.
 
-    [SerializeField]
-    float moveSpeed = 4f;
-    float perkele = 2f;
-    new Rigidbody rigidbody;
-    Vector3 forward, right;
-    public GameObject Player;
+    Vector3 movement;                   // The vector to store the direction of the player's movement.
+   // Animator anim;                      // Reference to the animator component.
+    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+    float camRayLength = 100f;          // The length of the ray from the camera into the scene.
 
-    void Start()
+    void Awake()
     {
-        forward = Camera.main.transform.forward;
-        forward.y = 0;
-        forward = Vector3.Normalize(forward);
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
-        
+        // Create a layer mask for the floor layer.
+        floorMask = LayerMask.GetMask("Floor");
+
+        // Set up references.
+        //anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    
-    void Update()   
+
+    void FixedUpdate()
     {
-        if (Input.anyKey)
-            Move();
-        
+        // Store the input axes.
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        // Move the player around the scene.
+        Move(h, v);
+
+        // Turn the player to face the mouse cursor.
+        Turning();
+
+        // Animate the player.
+       // Animating(h, v);
     }
 
-    void Move()
+    void Move(float h, float v)
     {
-        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
-        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
-        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+        // Set the movement vector based on the axis input.
+        movement.Set(h, 0f, v);
 
-       Vector3 heading = Vector3.ClampMagnitude(rightMovement + upMovement, moveSpeed);
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movement = movement.normalized * speed * Time.deltaTime;
 
-        transform.forward = heading;
-        
-
-
-        transform.position += rightMovement;
-        transform.position += upMovement;
-
+        // Move the player to it's current position plus the movement.
+        playerRigidbody.MovePosition(transform.position + movement);
     }
+
+    void Turning()
+    {
+        // Create a ray from the mouse cursor on screen in the direction of the camera.
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Create a RaycastHit variable to store information about what was hit by the ray.
+        RaycastHit floorHit;
+
+        // Perform the raycast and if it hits something on the floor layer...
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+            Vector3 playerToMouse = floorHit.point - transform.position;
+
+            // Ensure the vector is entirely along the floor plane.
+            playerToMouse.y = 0f;
+
+            // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+            // Set the player's rotation to this new rotation.
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+   /* void Animating(float h, float v)
+    {
+        // Create a boolean that is true if either of the input axes is non-zero.
+        bool walking = h != 0f || v != 0f;
+
+        // Tell the animator whether or not the player is walking.
+        anim.SetBool("IsWalking", walking);
+    }
+    */
 }
+
--- HackNSlash/Assets/Scripts/Player Controller/FixedCamera.cs
+++ HackNSlash/Assets/Scripts/Player Controller/FixedCamera.cs
@@ -3,17 +3,23 @@ using System.Collections;
 
 public class FixedCamera : MonoBehaviour
 {
+    public Transform target;            // The position that that camera will be following.
+    public float smoothing = 5f;        // The speed with which the camera will be following.
 
-    public Transform target;
-    public float smoothTime = 0.3F;
+    Vector3 offset;                     // The initial offset from the target.
 
-    private Vector3 velocity = Vector3.zero;
-
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
+        // Calculate the initial offset.
+        offset = transform.position - target.position;
+    }
 
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(target.position.x - 15, target.position.y, target.position.z - 15), ref velocity, smoothTime);
+    void FixedUpdate()
+    {
+        // Create a postion the camera is aiming for based on the offset from the target.
+        Vector3 targetCamPos = target.position + offset;
 
+        // Smoothly interpolate between the camera's current position and it's target position.
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
     }
 }
