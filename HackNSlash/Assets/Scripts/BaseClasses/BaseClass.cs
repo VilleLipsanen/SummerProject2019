8b4d4a14bceabf15602e75eccbe368faea1aa024
Lasse <34071995+p3kka123@users.noreply.github.com>
2019-05-14 11:35:10 +0300

    Base classes created
    Created base class for attack, magic and unit

---
+++ HackNSlash/Assets/Scripts/BaseClasses/BaseClass.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseClass : MonoBehaviour
+{
+
+    int HP;
+    int curHP;
+
+    int Stamina;
+    int curStamina;
+
+    int Speed;
+    int curSpeed;
+
+    int Mana;
+    int curMana;
+
+    int Atk;
+    int curAtk;
+
+    int Def;
+    int curDef;
+
+    int Crit;
+    int curCrit;
+
+    int Level;
+    int Exp;
+
+    List<BaseAttack> attacks;
+    List<BaseMagic> magics;
+
+}
